66f927f7b045d1b0b0e3617376ebbda7de46ec04
amir.goriya@tbaytel.com <amir.goriya@tbaytel.com>
2023-01-06 09:30:15 -0500

    Tooltip function is broken. Commented it out for now.

--- js/custom_compilation.js
+++ js/custom_compilation.js
@@ -101,7 +101,7 @@ $(function () {
     /*****************************************************************/
 
     /**ENABLE Popper.js/Bootstrap TOOLTIPS******************************************************/
-    $('[data-toggle="tooltip"]').tooltip();
+    //$('[data-toggle="tooltip"]').tooltip();
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl);
