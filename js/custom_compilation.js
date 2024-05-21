421c6beae74b7493183eea7346e951f9fa538cde
Angry Gorilla <67566141+GroggyGorilla@users.noreply.github.com>
2024-05-21 12:44:12 -0400

    Mobile JS update.

--- js/custom_compilation.js
+++ js/custom_compilation.js
@@ -116,7 +116,7 @@ $(function () {
         '<span class="ico-b-b"></span>'
     ];
 
-    $('.postbody').each(function () {
+    $('.postbody, .post-content').each(function () {
         var posthtml = "";
         posthtml = $(this).html();
         for (var i = 0; i < tokenBBCode.length; i++) {
